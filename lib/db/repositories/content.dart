5a05b3666467f6f0e29215204cc58f1baed55df6
jason.lakes.id <jasonelington.lakes.id@gmail.com>
2026-06-17 10:08:24 +0700

    changed box to public

--- lib/db/repositories/content.dart
+++ lib/db/repositories/content.dart
@@ -2,13 +2,13 @@ import '../models/content.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
 
 class NadagramContentRepository {
-  Box<NadagramContent> get _box => Hive.box<NadagramContent>('Content');
+  Box<NadagramContent> get box => Hive.box<NadagramContent>('Content');
 
   Future<void> addNewContent(NadagramContent content) async {
-    await _box.add(content);
+    await box.add(content);
   }
 
   List<NadagramContent> getAll() {
-    return _box.values.toList();
+    return box.values.toList();
   }
 }
