693ea19f8d37a9b3bd5a6e0530726b6090cb4da0
jason.lakes.id <jasonelington.lakes.id@gmail.com>
2026-06-18 14:53:40 +0700

    turned to private object

--- lib/db/repositories/content.dart
+++ lib/db/repositories/content.dart
@@ -2,38 +2,38 @@ import '../models/content.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
 
 class NadagramContentRepository {
-  Box<NadagramContent> get box => Hive.box<NadagramContent>('Content');
+  Box<NadagramContent> get _box => Hive.box<NadagramContent>('Content');
 
   Future<void> addNewContent(NadagramContent content) async {
-    await box.add(content);
+    await _box.add(content);
   }
 
   Future<void> addLikes(int index) async {
-    final content = box.get(index);
+    final content = _box.get(index);
 
     if (content == null) return;
 
     content.likeCount = content.likeCount + 1;
 
-    await box.put(index, content);
+    await _box.put(index, content);
   }
 
   Future<void> removeLikes(int index) async {
-    final content = box.get(index);
+    final content = _box.get(index);
 
     if (content == null) return;
 
     content.likeCount = content.likeCount - 1;
 
-    await box.put(index, content);
+    await _box.put(index, content);
   }
 
   List<NadagramContent> getAll() {
-    return box.values.toList();
+    return _box.values.toList();
   }
 
   List<NadagramContent> getContents(String keyword) {
-    final contents = box.values.toList();
+    final contents = _box.values.toList();
 
     final filteredContent = contents.where(
       (content) {
